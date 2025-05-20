da99b2b367293060510b236a2a2554bab535c3ab
1nv3n7or <1nv3n7or@gmail.com>
2025-05-20 18:37:06 +0300

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const supported = ["javascript", "javascriptreact", "vue", "typescript"];
+
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vsc-easy-log" is now active!')
+  let disposable = vscode.commands.registerCommand("vsc-easy-log.log", () => {
+    const editor = vscode.window.activeTextEditor;
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('vsc-easy-log.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from vsc-easy-log!')
-  })
+    if (!editor) {
+      vscode.window.showErrorMessage("No active editor");
 
-  context.subscriptions.push(disposable)
+      return;
+    }
+
+    const selection = editor.selection;
+    const selectedText = editor.document.getText(selection).trim();
+
+    if (!selectedText) {
+      vscode.window.showWarningMessage("No variable selected");
+
+      return;
+    }
+
+    if (!supported.includes(editor.document.languageId)) {
+      vscode.window.showErrorMessage(
+        `${editor.document.languageId} language is not supported`
+      );
+
+      return;
+    }
+
+    if (!/^[a-zA-Z_$][a-zA-Z_$0-9]*$/.test(selectedText)) {
+      vscode.window.showWarningMessage("Invalid variable name selected");
+
+      return;
+    }
+
+    const debugLine = `\nconsole.log("debug", ${selectedText});\n`;
+
+    editor.edit((editBuilder) => {
+      // Insert after the current line
+      const position = new vscode.Position(
+        selection.end.line,
+        Number.MAX_SAFE_INTEGER
+      );
+      editBuilder.insert(position, debugLine);
+    });
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
